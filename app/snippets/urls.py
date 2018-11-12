1797070e52854264a23578d3ed1c230c05c278d5
ksw <zenuine1@gmail.com>
2018-11-12 16:16:35 +0900

    snippet_detail api구현

--- app/snippets/urls.py
+++ app/snippets/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('snippets/', views.snippet_list)
+    path('snippets/', views.snippet_list),
+    path('snippets/<int:pk>/', views.snippet_detail),
 ]
