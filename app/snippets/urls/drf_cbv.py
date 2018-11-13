13db8b4780c36187ec7d7f8793fa9189ca86c875
ksw <zenuine1@gmail.com>
2018-11-13 12:26:57 +0900

    SnippetDetail CBV구현

--- app/snippets/urls/drf_cbv.py
+++ app/snippets/urls/drf_cbv.py
@@ -4,6 +4,7 @@ from rest_framework.urlpatterns import format_suffix_patterns
 from ..views import drf_cbv as views
 
 urlpatterns = [
-    path('snippets/', views.SnippetList.as_view())
+    path('snippets/', views.SnippetList.as_view()),
+    path('snippets/<int:pk>/', views.SnippetDetail.as_view()),
 ]
 urlpatterns = format_suffix_patterns(urlpatterns)
