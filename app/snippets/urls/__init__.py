71599892c08951d325bf57608648de7faadfeaad
ksw <zenuine1@gmail.com>
2018-11-13 14:10:49 +0900

    Mixin기법 사용

--- app/snippets/urls/__init__.py
+++ app/snippets/urls/__init__.py
@@ -1,9 +1,10 @@
 from django.urls import path, include
 
-from . import django_fbv, drf_fbv, drf_cbv
+from . import django_fbv, drf_fbv, drf_cbv, drf_mixin
 
 urlpatterns = [
     path('django-fbv/', include(django_fbv)),
     path('drf-fbv/', include(drf_fbv)),
     path('drf-cbv/', include(drf_cbv)),
+    path('drf-mixin/', include(drf_mixin)),
 ]
